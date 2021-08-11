a3763b5cc70bd99c5b08f4f3c10088c5a50c7722
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-11 14:53:11 +0800

    add unimplemented function get_books_within_range

--- pyspark_scripts/price.py
+++ pyspark_scripts/price.py
@@ -7,3 +7,6 @@ def get_max_revenue(spark_dataframe):
         "total_price",f.col("price") * f.col("books_count")
         ).select("total_price").groupBy().sum().collect()[0][0]
     return {"sum": f"{result:.2f}"}
+
+def get_books_within_range(spark_dataframe, min, max):
+    pass
