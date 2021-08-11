eb26f3b63844ed1f64433ef4f7d707ae2ff5f55f
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-11 11:25:12 +0800

    add failing test for get_average_rating

---
+++ tests/test_pyspark.py
@@ -0,0 +1,42 @@
+import pytest
+from tests import SPARK
+
+@pytest.fixture
+def df_columns():
+    columns = [
+        'id',
+        'author',
+        'title',
+        'image_url',
+        'small_image_url',
+        'price',
+        'books_count',
+        'isbn',
+        'isbn13',
+        'original_publication_year',
+        'original_title',
+        'language_code',
+        'average_rating'
+    ]
+    return columns
+
+def test_should_return_average_rating(df_columns):
+
+    test_data = [
+        [51,"author1","book1","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",2.00],
+        [53,"author2","book2","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",3.00],
+        [54,"author3","book3","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",4.00]
+            ]
+
+    test_dataframe = SPARK.createDataFrame(test_data, df_columns)
+
+    actual = get_average_rating(test_dataframe)
+
+    expected = 4.00
+
+    assert actual == expected
+
+
+
+
+
