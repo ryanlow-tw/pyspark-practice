3edf9c04b19150832518173268e0ec9015189e01
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-11 12:25:40 +0800

    add failing test for get_highly_rated_books

--- tests/test_pyspark.py
+++ tests/test_pyspark.py
@@ -50,9 +50,31 @@ def test_should_return_average_rating_format_to_2dp(df_columns) -> None:
 
     actual = get_average_rating(test_dataframe)
 
-    expected = 4.20
+    expected = 4.22
 
     assert actual == expected
+
+def test_should_return_highly_rated_books(df_columns) -> None:
+
+    test_data = [
+        [51,"author1","low_rated_book1","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",1.0],
+        [53,"author2","low_rated_book2","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",2.0],
+        [54,"author3","high_rated_book3","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",5.0]
+            ]
+
+    test_dataframe = SPARK.createDataFrame(test_data, df_columns)
+
+    actual = get_highly_rated_books(test_dataframe)
+
+    expected = {}
+    expected_data = [54,"author3","high_rated_book3","test_url","test_url",1461,178,1416914285,9.78142E+12,2007,"City of Bones","eng",5.0]
+
+    for i in range(len(df_columns)):
+        expected[df_columns[i]] = expected_data[i]
+
+    assert actual == expected
+
+
 
 
 
