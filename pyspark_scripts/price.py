389a132c6e8d1cb3c7a84fc58d75bb0ad43ef5c0
ryanlow-tw <ryan.low@thoughtworks.com>
2021-08-16 11:34:51 +0800

    refactor get_max_revenue to not use collect[0][0]

--- pyspark_scripts/price.py
+++ pyspark_scripts/price.py
@@ -5,9 +5,10 @@ def get_max_revenue(spark_dataframe):
 
     result = spark_dataframe.withColumn(
         "total_price",f.col("price") * f.col("books_count")
-        ).select("total_price").groupBy().sum().collect()[0][0]
-    return {"sum": f"{result:.2f}"}
+        ).agg(f.sum('total_price').alias("total_revenue")).first().total_revenue
 
+    return {"sum": f"{result:.2f}"}
+    
 def get_books_within_range(spark_dataframe, min_val, max_val):
     results = spark_dataframe.filter(
         (spark_dataframe["price"] >= min_val) & (spark_dataframe["price"] < max_val)
